30d73d782896ce4db0fe95f8855345d032adf94a
Samson Cain <samsonc99@gmail.com>
2023-01-20 08:58:29 -0600

    fix: added guard for tokens if length is 0

--- src/notifications/sendPushNotification.ts
+++ src/notifications/sendPushNotification.ts
@@ -5,18 +5,29 @@ import type { User } from "../types";
 import { USER_COLLECTION } from "../index";
 
 interface ISendPushNotification {
-  players: string | string[];
+  playerIds: string | string[];
   title: string;
   body: string;
 }
 
+/**
+ * A function that takes care of getting users FCM tokens and then sends notifications
+ *  to the specified users
+ *
+ * @param An object containing
+ *      1. ID or IDs of players to notify
+ *      2. title of notification
+ *      3. body of notification
+ */
 const sendPushNotification = async ({
-  players,
+  playerIds,
   title,
   body,
 }: ISendPushNotification) => {
   const MESSAGING = getMessaging();
-  const tokens = await getPlayerFCMTokens(players);
+  const tokens = await getPlayerFCMTokens(playerIds);
+
+  if (tokens.length === 0) return;
 
   const message: MulticastMessage = {
     notification: {
@@ -37,11 +48,11 @@ const sendPushNotification = async ({
  * @returns FCM token or array of FCM tokens
  */
 const getPlayerFCMTokens = async (
-  players: string | string[]
+  playerIds: string | string[]
 ): Promise<string[]> => {
-  if (Array.isArray(players)) {
+  if (Array.isArray(playerIds)) {
     // Query to get users with uid in the game
-    const userQuery = USER_COLLECTION.where("uid", "in", players);
+    const userQuery = USER_COLLECTION.where("uid", "in", playerIds);
 
     // Get docs
     const userDocs = await userQuery.get().then((querySnap) => querySnap.docs);
@@ -49,7 +60,7 @@ const getPlayerFCMTokens = async (
     const users = userDocs.map((doc) => doc.data()) as User[];
     return users.map(({ fcmToken }) => fcmToken);
   } else {
-    const userDoc = USER_COLLECTION.doc(players);
+    const userDoc = USER_COLLECTION.doc(playerIds);
     const userData = (await userDoc.get().then((doc) => doc.data())) as User;
     return [userData.fcmToken];
   }
